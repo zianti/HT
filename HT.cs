0672b4876bc99044dcea5ec2d96783d15f2fd433
zianti <xidnos@gmail.com>
2019-10-27 03:28:01 +0300

    Killer

--- HT.cs
+++ HT.cs
@@ -20,7 +20,11 @@ public class HT : PhysicsGame
         Level.BackgroundColor = Color.Beige;
         Level.CreateBorders();
 
-        PhysicsObject pelaaja = LuoNelikulmio(this, 50, "pelaaja1");
+        PhysicsObject pelaaja = LuoNelikulmio(this, "pelaaja1");
+        PhysicsObject vesa = LuoNelikulmio(this, "vesa");
+        PhysicsObject kyna = Kynat(this);
+
+        AddCollisionHandler(pelaaja, kyna, KasittelePallonTormays);
 
         Keyboard.Listen(Key.F1, ButtonState.Pressed, ShowControlHelp, "Näytä");
         Keyboard.Listen(Key.Up, ButtonState.Down, LyoUkkoa, "lyö", pelaaja, nopeusYlos);
@@ -28,11 +32,13 @@ public class HT : PhysicsGame
         Keyboard.Listen(Key.Right, ButtonState.Down, LyoUkkoa, "lyö oi", pelaaja, nopeusOikealle);
         Keyboard.Listen(Key.Left, ButtonState.Down, LyoUkkoa, "lyö v", pelaaja, nopeusVasemmalle);
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, Exit, "Lopeta peli");
+
         
+
 
     }
 
-    public static PhysicsObject LuoNelikulmio(PhysicsGame peli, double vauhti, string tunniste)
+    public static PhysicsObject LuoNelikulmio(PhysicsGame peli, string tunniste)
     {
         PhysicsObject ukko = new PhysicsObject(70, 100, Shape.Rectangle);
         ukko.Color = Color.Black;
@@ -55,5 +61,31 @@ public class HT : PhysicsGame
         ukko.Push(suunta);
     }
 
+    public static PhysicsObject Kynat(PhysicsGame peli)
+    {
+        PhysicsObject kyna = new PhysicsObject(20, 40);
+        kyna.Color = Color.Red;
+        peli.Add(kyna);
+        kyna.Y = 100;
+        kyna.X = 100;
+        Vector suunta = RandomGen.NextVector(100, 200);
+        kyna.Hit(suunta);
 
+    
+
+        return kyna;
+    }
+
+
+
+    void KasittelePallonTormays(PhysicsObject pelaaja, PhysicsObject kyna)
+    {
+        Explosion rajahdys = new Explosion(kyna.Width * 2);
+        rajahdys.Position = kyna.Position;
+        rajahdys.UseShockWave = false;
+        this.Add(rajahdys);
+        Remove(kyna);
+    }
+
+    // Moro Valte
 }
